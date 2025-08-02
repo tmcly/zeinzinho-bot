0d36459e5c997e2d9ae63f9e987cf09de7c2c626
RhMacley <rh.macley@gmail.com>
2025-08-02 12:11:51 -0300

    Initial commit

---
+++ src/main/java/com/zeinzinho/zeinzinho_bot/ZeinzinhoBotApplication.java
@@ -0,0 +1,13 @@
+package com.zeinzinho.zeinzinho_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeinzinhoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeinzinhoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeinzinho/zeinzinho_bot/ZeinzinhoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeinzinho.zeinzinho_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeinzinhoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
