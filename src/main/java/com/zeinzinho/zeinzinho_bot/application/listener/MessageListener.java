bc5edd5b0ec230d11eec2ce2959c3aa916fb8cf2
RhMacley <rh.macley@gmail.com>
2025-08-06 13:30:36 -0300

    fix: remove bad behavior

--- src/main/java/com/zeinzinho/zeinzinho_bot/application/listener/MessageListener.java
+++ src/main/java/com/zeinzinho/zeinzinho_bot/application/listener/MessageListener.java
@@ -1,6 +1,5 @@
 package com.zeinzinho.zeinzinho_bot.application.listener;
 
-import java.util.concurrent.ThreadLocalRandom;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
@@ -23,22 +22,8 @@ public class MessageListener extends ListenerAdapter {
 
     if (messageContent.equalsIgnoreCase("!help")) {
       event.getChannel().sendMessage(
-          "Um bot que nem o GabrielZein, linguiçudo e faz de tudo um pouco, no momento converte links de vídeos das principais plataformas para versões Embed pra dentro do discord, use e abuse a vontade como se fosse o GabrielZein.")
+          "Um bot que nem o nosso amigo GabrielZein, faz de tudo um pouco, no momento converte links de vídeos das principais plataformas para versões Embed pra dentro do discord.")
           .queue();
-    }
-
-    if (messageContent.contains("normal")) {
-      String[] responses = {
-          "Mas e o gay?",
-          "Entendi, mas e o gay?",
-          "Legal, mas e o gay?",
-          "Interessante, mas e o gay?",
-          "Ok, mas e o gay?",
-          "Certo certo, mas e o gay?",
-          "Sei, mas e o gay?"
-      };
-      int idx = ThreadLocalRandom.current().nextInt(responses.length);
-      event.getChannel().sendMessage(responses[idx]).queue();
     }
   }
 }
